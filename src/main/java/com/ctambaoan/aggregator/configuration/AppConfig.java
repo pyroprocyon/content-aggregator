01259d4957c1c079a4b5adec485418fdd7a6adea
Procyon <pyroprocyon02@gmail.com>
2025-09-01 12:58:15 +0800

    Use @EnableSpringDataWebSupport for simplified representation of Page instance

--- src/main/java/com/ctambaoan/aggregator/configuration/AppConfig.java
+++ src/main/java/com/ctambaoan/aggregator/configuration/AppConfig.java
@@ -3,14 +3,18 @@ package com.ctambaoan.aggregator.configuration;
 import com.ctambaoan.aggregator.connector.NewsSourceEnum;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
+import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
+
 @Configuration
 @EnableAsync
+@EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
 public class AppConfig {
 
   @Bean
